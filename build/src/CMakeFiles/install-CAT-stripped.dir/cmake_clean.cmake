0d0d061374ddc9b916b4d30bfa4867a97b86c249
Kevin Alexander McAfee <kam9807@peroni.cs.northwestern.edu>
2021-05-08 16:38:43 -0500

    Cleaning

--- build/src/CMakeFiles/install-CAT-stripped.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-CAT-stripped"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-CAT-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
